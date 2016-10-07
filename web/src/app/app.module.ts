eb867438b4b56a5c1d6959f831850151427ae8f7
Benoit Hediard <ben@agorapulse.com>
2016-10-07 13:20:18 +0200

    Upgrade angular to 2.0.2 (for core and web)

--- web/src/app/app.module.ts
+++ web/src/app/app.module.ts
@@ -1,10 +1,9 @@
 import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
 import { StoreModule } from '@ngrx/store';
-import { EffectsModule } from '@ngrx/effects';
 import { StoreDevtoolsModule } from '@ngrx/store-devtools';
 
-import { CounterEffects, CounterModule } from 'ngrx-demo-core';
+import { CounterModule } from 'ngrx-demo-core';
 import { reducer } from './app.reducer';
 import { AppComponent } from './app.component';
 
